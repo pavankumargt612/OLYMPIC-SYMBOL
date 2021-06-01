c0696fafc873b3e3e99d55017a10217f7f6e65c1
pavankumargt612 <79921778+pavankumargt612@users.noreply.github.com>
2021-06-01 15:48:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
-canvas=document.getElementById("myCanvas");
+canvas = document.getElementById("myCanvas");
 ctx=canvas.getContext("2d");
-color="red";
 
 ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=2
-ctx.arc(200,200,40,0,2*Math.PI);
+ctx.strokeStyle = "grey";
+ctx.lineWidth=20;
+ctx.rect(135,110,430,250);
 ctx.stroke();
 
-canvas.addEventListener("mousedown",my_mousedown);
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth=10;
+ctx.arc(250,210,40,0,2 * Math.PI);
+ctx.stroke();
 
-function my_mousedown(e){
-mouse_x=e.clientX-canvas.offsetLeft;
-mouse_y=e.clientY-canvas.offsetTop;
-circle(mouse_x,mouse_y);
-}
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth=10;
+ctx.arc(350,210,40,0,2 * Math.PI);
+ctx.stroke();
 
-function circle(mouse_x,mouse_y){
-    ctx.beginPath();
-    ctx.strokeStyle=color;
-    ctx.lineWidth=2
-    ctx.arc(mouse_x,mouse_y,40,0,2*Math.PI);
-    ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth=10;
+ctx.arc(450,210,40,0,2 * Math.PI);
+ctx.stroke();
 
-}
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth=10;
+ctx.arc(300,250,40,0,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth=10;
+ctx.arc(400,250,40,0,2 * Math.PI);
+ctx.stroke();
